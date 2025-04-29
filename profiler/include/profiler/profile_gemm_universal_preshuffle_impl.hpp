3c10c33121084c47614c9972acbb969a91440bb7
Nitin Srinivasan <Nitin.Srinivasan@amd.com>
2025-04-29 14:17:10 -0700

    Update profile_gemm_universal_preshuffle_impl.hpp

--- profiler/include/profiler/profile_gemm_universal_preshuffle_impl.hpp
+++ profiler/include/profiler/profile_gemm_universal_preshuffle_impl.hpp
@@ -144,7 +144,7 @@ bool profile_gemm_universal_impl(int do_verification,
 
     a_device_buf.ToDevice(a_m_k.mData.data());
 
-    using DeviceOp = ck::tensor_operation::device::DeviceGemm_Xdl_CShuffleV3_BPreshuffle<ALayout,
+    using DeviceOpInstance = ck::tensor_operation::device::DeviceGemm_Xdl_CShuffleV3_BPreshuffle<ALayout,
                                                                 BLayout,
                                                                 CLayout,
                                                                 ADataType,
