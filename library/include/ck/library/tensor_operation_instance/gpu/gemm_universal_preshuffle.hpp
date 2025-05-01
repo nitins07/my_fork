15e948c9cb53004ee7640b7e5984749ad29eee38
Nitin Srinivasan <Nitin.Srinivasan@amd.com>
2025-04-30 19:07:07 -0700

    Update gemm_universal_preshuffle.hpp

--- library/include/ck/library/tensor_operation_instance/gpu/gemm_universal_preshuffle.hpp
+++ library/include/ck/library/tensor_operation_instance/gpu/gemm_universal_preshuffle.hpp
@@ -131,7 +131,7 @@ struct DeviceOperationInstanceFactory<
         }
 #endif
 #endif // CK_USE_WMMA
-
+*/
 #ifdef CK_USE_XDL
 #ifdef CK_ENABLE_FP16
         if constexpr(is_same_v<ADataType, half_t> && is_same_v<BDataType, half_t> &&
@@ -259,7 +259,7 @@ struct DeviceOperationInstanceFactory<
             }
         }
 #endif
-*/
+
 #ifdef CK_ENABLE_BF16
         if constexpr(is_same_v<ADataType, bhalf_t> && is_same_v<BDataType, bhalf_t> &&
                      is_same_v<CDataType, bhalf_t>)
@@ -362,7 +362,7 @@ struct DeviceOperationInstanceFactory<
             }
         }
 #endif
-/*
+
 #if(defined(CK_ENABLE_BF16) && defined(CK_ENABLE_FP8))
         if constexpr(is_same_v<ADataType, f8_t> && is_same_v<BDataType, f8_t> &&
                      is_same_v<CDataType, bhalf_t>)
@@ -414,7 +414,7 @@ struct DeviceOperationInstanceFactory<
             }
         }
 #endif
-*/
+
 #ifdef CK_ENABLE_BF16
         if constexpr(is_same_v<ADataType, bhalf_t> && is_same_v<BDataType, pk_i4_t> &&
                      is_same_v<CDataType, bhalf_t>)
